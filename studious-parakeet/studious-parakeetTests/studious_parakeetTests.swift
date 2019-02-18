07336c192c1a3dc9aef4d44bf5045990446b7587
Harry Woodworth <hwoodwo1@bl-n28.pods.bu.int>
2019-02-18 10:35:15 -0500

    Create project

---
+++ studious-parakeet/studious-parakeetTests/studious_parakeetTests.swift
@@ -0,0 +1,36 @@
+//
+//  studious_parakeetTests.swift
+//  studious-parakeetTests
+//
+//  Created by Harry Woodworth on 2/18/19.
+//  Copyright © 2019 Harry Woodworth. All rights reserved.
+//
+
+import XCTest
+@testable import studious_parakeet
+
+class studious_parakeetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
