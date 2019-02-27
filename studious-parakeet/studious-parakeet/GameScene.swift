a7a5159fab250252362767f92ac439035a93db42
Harry Woodworth <hwoodwo1@bl-n28.pods.bu.int>
2019-02-27 16:31:54 -0500

    Add comment documentation

--- studious-parakeet/studious-parakeet/GameScene.swift
+++ studious-parakeet/studious-parakeet/GameScene.swift
@@ -30,22 +30,31 @@ struct PhysicsCategory {
 
 class GameScene: SKScene
 {
+    // Player sprite
     private var player = SKSpriteNode()
+    // Texture array for player animation
     private var playerBoostFrames: [SKTexture] = []
+    // Time between debris spawn
     private var debrisWaitDuration = 2.0
+    // Current Score
     private var score = 0
+    // Label to display score
     private var scoreLabel = SKLabelNode()
-    
+    // Preferences for storing/modifying high score
+    private let preferences = NSUserDefaults
+    // Current move action used by the player
     var currentAction: SKAction? = nil
     
     // Runs after the scene is presented to the view
     override func didMove(to view: SKView)
     {
+        // Set the background color
         backgroundColor = SKColor.black
         
         // Add player sprite
         buildPlayerSprite()
         
+        // Ignore gravity, set contact delegate to self for collision (uses extention at bottom)
         physicsWorld.gravity = .zero
         physicsWorld.contactDelegate = self
         
@@ -83,6 +92,8 @@ class GameScene: SKScene
         ))
     }
     
+    // Recursive loop that spawns debris using a changing debrisWaitDuration
+    // Recursive because we need to change the SKAction.wait duration each loop
     func debrisRecursiveLoop()
     {
         let recursive = SKAction.sequence([
@@ -99,20 +110,21 @@ class GameScene: SKScene
         // Create the animation using the atlas
         let playerBoostAtlas = SKTextureAtlas(named: "PlayerBoost")
         var boostFrames: [SKTexture] = []
-        
         let numImages = playerBoostAtlas.textureNames.count
         for i in 1...numImages {
             let textureName = "player-boost\(i)"
             boostFrames.append(playerBoostAtlas.textureNamed(textureName))
         }
         
+        // Set the animation to the player
         playerBoostFrames = boostFrames
         
-        // Set/Add player sprite
+        // Create player sprite
         player = SKSpriteNode(texture: playerBoostFrames[0])
         player.position = CGPoint(x: size.width / 2, y: size.height / 2)
         player.setScale(1.5)
-        
+
+        // Player phsyicsbody for collisions
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
         player.physicsBody?.isDynamic = true
         player.physicsBody?.categoryBitMask = PhysicsCategory.player
@@ -120,9 +132,11 @@ class GameScene: SKScene
         player.physicsBody?.collisionBitMask = PhysicsCategory.none
         player.physicsBody?.usesPreciseCollisionDetection = true
         
+        // Add to scene
         addChild(player)
     }
     
+    // Create and position the score label
     func buildScoreLabel()
     {
         scoreLabel = SKLabelNode(text: "Score: 0")
@@ -131,6 +145,7 @@ class GameScene: SKScene
         addChild(scoreLabel)
     }
     
+    // Animation loop for the player that runs forever
     func animatePlayerBoost()
     {
         player.run(
@@ -142,33 +157,26 @@ class GameScene: SKScene
             withKey:"PlayerBoosting")
     }
     
-    // Run all the actions added to the sequence and create stars
+    // Run the curent action (prevents action stacking / teleporting)
     func updateGame()
     {
         if(currentAction != nil) {
             player.run(currentAction!)
         }
-        //addStar()
     }
     
     // Add touches to the action sequence
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
     {
+        // Get the first touch
         guard let touch = touches.first else { return }
         let touchLoc = touch.location(in: self)
         
+        // Direction from player to touch
         let direction = player.position - touchLoc
         
+        // Set the move action (modified to make short moves not super slow)
         currentAction = SKAction.move(to: touchLoc, duration: 0.5 * Double(direction.length()/100) )
-    }
-    
-    // Random functions
-    func random() -> CGFloat {
-        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
-    }
-    
-    func random(min: CGFloat, max: CGFloat) -> CGFloat {
-        return random() * (max - min) + min
     }
     
     // Create debris objects
@@ -176,27 +184,34 @@ class GameScene: SKScene
     {
         let debris = SKSpriteNode(imageNamed: "Debris")
         
+        // Make it a random size
         let sizeRandom = random(min: 1.0, max: 5.0 )
         
+        // Set the scale
         debris.setScale(sizeRandom)
         
+        // Set the physics body
         debris.physicsBody = SKPhysicsBody(circleOfRadius: debris.size.width / 2)
         debris.physicsBody?.isDynamic = true
         debris.physicsBody?.categoryBitMask = PhysicsCategory.debris
         debris.physicsBody?.contactTestBitMask = PhysicsCategory.player
         debris.physicsBody?.collisionBitMask = PhysicsCategory.none
         
+        // Random position on X axis
         let randomX = random(min: 0, max: size.width)
         debris.position = CGPoint(x: randomX, y: size.height + debris.size.height)
         
+        // Add to scene
         addChild(debris)
         
+        // Random speed move action down screen
         let randomDuration = random(min: CGFloat(2.0), max: CGFloat(6.0))
-        
         let moveAction = SKAction.move(to: CGPoint(x: randomX, y: -debris.size.height), duration: TimeInterval(randomDuration))
         
+        // Delete action
         let deleteAction = SKAction.removeFromParent()
         
+        // Run actions
         debris.run(SKAction.sequence([moveAction,deleteAction]))
 
     }
@@ -206,47 +221,68 @@ class GameScene: SKScene
     {
         let star = SKSpriteNode(imageNamed: "Star")
         
+        // Random size
         star.setScale(random(min: 0.5, max: 4.0 ))
         
+        // Random position on x axis
         let randomX = random(min: 0, max: size.width)
         star.position = CGPoint(x: randomX, y: size.height + star.size.height)
         
+        // Add to scene
         addChild(star)
         
+        // Random speed move down screen
         let randomDuration = random(min: CGFloat(2.0), max: CGFloat(6.0))
-        
         let moveAction = SKAction.move(to: CGPoint(x: randomX, y: -star.size.height), duration: TimeInterval(randomDuration))
         
+        // Delete action
         let deleteAction = SKAction.removeFromParent()
         
+        // Run actions
         star.run(SKAction.sequence([moveAction,deleteAction]))
     }
     
     // Increase the score and decrease the debris wait duration
     func scoreDebrisMod()
     {
+        // Increment score
         score += 1
+        // Update score label text
         scoreLabel.text = "Score: \(score)"
         
+        // Decrement debrisWaitDuration, min in 0.3
         if(debrisWaitDuration > 0.3) {
             debrisWaitDuration -= 0.02
         } else {
+            // Fixes negative jump number glitch
             debrisWaitDuration = 0.3
         }
     }
     
+    // Called by extention when a collision between player and debris happens
     func playerCollidedWithDebris(player: SKSpriteNode, debris: SKSpriteNode)
     {
+        // Reset score and debris wait duration
         score = 0
         debrisWaitDuration = 2.0
     }
     
+    // Random calculation helper functions
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
 }
 
+// Extension for collision detection
 extension GameScene: SKPhysicsContactDelegate
 {
     func didBegin(_ contact: SKPhysicsContact)
     {
+        // Detect a collision
         var firstBody: SKPhysicsBody
         var secondBody: SKPhysicsBody
         if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
@@ -257,6 +293,7 @@ extension GameScene: SKPhysicsContactDelegate
             secondBody = contact.bodyA
         }
         
+        // Call playerCollidedWithDebris
         if ((firstBody.categoryBitMask & PhysicsCategory.debris != 0) &&
             (secondBody.categoryBitMask & PhysicsCategory.player != 0)) {
             if let debris = firstBody.node as? SKSpriteNode,
